3f923cfad89de0cd093fdf90020481009d43a8b2
Samuelpaz95 <samuelpaz243@gmail.com>
2019-12-23 12:11:20 -0400

    Corrigiendo fallos

--- camara.py
+++ camara.py
@@ -2,10 +2,12 @@ import cv2
 import numpy as np
 
 from face_identity.Identificador import Identificador
+from face_identity.kmean import K_mean
 
 
 print('Cargando modelo de reconicimiento facial')
-identificador = Identificador(name = "prueba")
+identificador = Identificador(name = "scesi_auth")
+k_mean = K_mean('pruebas_1_km')
 
 FACE_CASCADE = cv2.CascadeClassifier('face_identity/haarcascade_frontalface_default.xml')
 EYE_CASCADE = cv2.CascadeClassifier('face_identity/haarcascade_eye.xml')
@@ -29,30 +31,57 @@ def camara(ip=None):
         active = CAP.open(0)
     else:
         active = CAP.open(ip+'/video')
-    input_images = []
+    input_image_codes = []
     nombre = "Detectando..."
     while active:
         _, frame = CAP.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face_boxes, encode_images = identificador.box_and_encode(frame)
+        
+        media = []
+        for code in input_image_codes:
+            media.append(np.median(code, axis=0))
+        media = np.array(media)
+        
+        if len(input_image_codes) == 0 and len(encode_images) > 0:
+            input_image_codes = list(encode_images.reshape((len(encode_images), 1, 128)))
+            #print(input_image_codes)
+        elif len(encode_images) == len(input_image_codes):
+            
+            for face_box, code in zip(face_boxes, encode_images):
+                index = np.linalg.norm(media - code, axis=1).argmin()
+                input_image_codes[index] = np.concatenate([ input_image_codes[index], [code] ])
+                nombre = 'Detectando...'
+                if len(input_image_codes[index]) > 10:
+                    input_code = np.median(input_image_codes[index], axis=0)
+                    #input_image_codes[index] = (input_image_codes[index] + input_code) / 2
+                    input_image_codes[index] = input_image_codes[index][1:]
+                    #print(input_code)
+                    nombre = str(k_mean.that_class(input_code))
+                (x1, y1), (x2, y2) = face_box
+                cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0))
+                cv2.putText(frame, str(nombre), (x1,y1+10), font, 1,(0,0,255), 2, cv2.LINE_AA)
+        
+        elif len(encode_images) < len(input_image_codes):
+            indices = list(range(len(input_image_codes)))
+            
+            for face_box, code in zip(face_boxes, encode_images):
+                index = np.linalg.norm(media - code, axis=1).argmin()
+                _ = indices.pop(index)
+                
+            for index in indices:
+                _ = input_image_codes.pop(index)
+                
+        
+        else:
+            distances = []
+            for code in encode_images:
+                distance = np.linalg.norm(media - code, axis=1).min()
+                distances.append(distance)
+            index = np.argmax(distances)
+            new_code = encode_images[index].reshape((1, 128))
+            input_image_codes.append(new_code)
 
-        faces = FACE_CASCADE.detectMultiScale(
-            gray,
-            scaleFactor=1.2,
-            minNeighbors=6,
-            minSize=(96,96),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-        for x,y,w,h in faces:
-            x1 = x
-            y1 = y
-            x2 = x+w
-            y2 = y+h
-            input_images.append(frame)
-            if len(input_images) > 5:
-                nombre = identificador.identify(input_images)
-                input_images = []
-            cv2.putText(frame, str(nombre), (x1,y1+10), font, 1,(0,0,255), 2, cv2.LINE_AA)
-            cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0))
         cv2.imshow('CAMARA', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -78,38 +107,37 @@ def register_camera(id_user, ip = None):
         active = CAP.open(0)
     else:
         active = CAP.open(ip+'/video')
-    images = []
+    input_codes = []
+    mjs = 'Preciona "q" para registrar su rostro'
+    print(mjs)
+    
     while active and id_user.isnumeric():
-        mjs = 'Preciona "q" para registrar su rostro'.
-        print(mjs)
         _, frame = CAP.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.equalizeHist(gray)
-        faces = FACE_CASCADE.detectMultiScale(
-            gray,
-            scaleFactor=1.2,
-            minNeighbors=6,
-            minSize=(96,96),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-        if len(faces) == 1:
-            faces = faces[0]
-            x1 = faces[0]
-            y1 = faces[1]
-            x2 = faces[0]+faces[2]
-            y2 = faces[1]+faces[3]
+        frame[:50] = np.array([0, 0, 255])
+        frame[:50, : int(frame.shape[1] * (len(input_codes) / 100))] = np.array([255,0,0])
+        
+        face_boxes, encode_images = identificador.box_and_encode(frame)
+        
+        if len(encode_images) == 1 and len(input_codes) < 100:
+            input_codes.append(encode_images[0])
+            (x1, y1), (x2, y2) = face_boxes[0]
             cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0))
-            images.append(frame)
-            if len(images) > 30:
-                images.remove(images[0])
-        cv2.imshow('Camara', frame)
+        
+        elif len(input_codes) >= 100:
+            median = np.median(input_codes, axis=0)
+            input_codes = list( (np.array(input_codes) + median) / 2 )[:99]
+        
+        cv2.imshow('camara', frame)
         if (cv2.waitKey(1) & 0xFF == ord('q')):
-            images = np.array(images)
-            print(images.shape)
-            res = identificador.registrar_usuario(id_user, images)
-            mjs = 'Su registro se completo exitosamente'
+            k_mean.add_class(id_user, np.array(input_codes))
+            mjs = f'Su registro se completo exitosamente con {id_user}'
             break
     print(mjs)
     CAP.release()
     cv2.destroyAllWindows()
     return res
+
+if __name__ == "__main__":
+    camara('http://192.168.1.159:4040')
+    #camara()
+    #register_camera('1234', 'http://192.168.1.159:4040')
